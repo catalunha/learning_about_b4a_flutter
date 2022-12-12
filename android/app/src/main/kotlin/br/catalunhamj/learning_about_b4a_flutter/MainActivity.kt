e2a780502cbd68a5b2ce267f861596dd8792c355
catalunha <catalunha.mj@gmail.com>
2022-12-12 16:01:20 -0300

    first commit

---
+++ android/app/src/main/kotlin/br/catalunhamj/learning_about_b4a_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.catalunhamj.learning_about_b4a_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ bin/learning_about_b4a_dart.dart
@@ -0,0 +1,38 @@
+import 'dart:io';
+
+void main(List<String> arguments) async {
+  joinPostsInReadme();
+}
+
+void joinPostsInReadme() {
+  String pathREADME = 'README.md';
+  var fileREADME = File(pathREADME).openWrite(mode: FileMode.write);
+  List<String> parts = [
+    // 'introducao',
+    // 'apresentacao',
+    // 'tabelas',
+    // 'database',
+    // 'app',
+    // 'genre_dados',
+    // 'genre_buscas',
+    // 'author_dados',
+    // 'author_buscas',
+    // 'shape_dados',
+    // 'shape_buscas',
+    // 'publisher_dados',
+    // 'publisher_buscas',
+    // 'book_dados',
+    // 'book_buscas',
+  ];
+  for (var part in parts) {
+    String pathPart = 'readmes/$part.md';
+    var dataPart = File(pathPart).readAsStringSync();
+    fileREADME.writeln('<a id="$part"></a>');
+    fileREADME.writeln(dataPart);
+    fileREADME.writeln('[Voltar ao sumário](#sumario)');
+    fileREADME.writeln();
+    fileREADME.writeln('---');
+    fileREADME.writeln('---');
+  }
+  fileREADME.close();
+}
